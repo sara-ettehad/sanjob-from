99aa3d062dbff24733df8e60f8740d23c0ac6f88
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2024-05-20 01:07:11 -0400

    message

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink, Link } from "react-router-dom";
+
+
+class Home extends React.Component{
+    render(){
+        return (
+            <main>
+
+                
+            </main>
+        );
+    }
+}
+
+export default Home;
